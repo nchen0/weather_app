b94fbcc72f9d54a92ae96d7740bf3cd55a12eef3
Nicky Chen <nicky.chen0@gmail.com>
2018-11-21 19:21:30 -0800

    cors fix

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
           `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyDEPcm9glqHYP2SkAubicuE9A4pPlsZjI0`
         ),
         axios.get(
-          `https://api.darksky.net/forecast/1ea2d54ed66f54e9be6e30f638711be9/${latitude},${longitude}`
+          `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/1ea2d54ed66f54e9be6e30f638711be9/${latitude},${longitude}`
         )
       ])
       .then(
@@ -39,6 +39,14 @@ class App extends Component {
     navigator.geolocation.getCurrentPosition(this.getCity);
   };
 
+  getIcon = descriptor => {
+    console.log("descriptor is: ", descriptor);
+    switch (descriptor) {
+      case "partly-cloudy-night":
+        return "PARTLY_CLOUDY_NIGHT";
+    }
+  };
+
   render() {
     const defaults = {
       icon: "CLEAR_DAY",
@@ -49,7 +57,7 @@ class App extends Component {
     let today = new Date();
     let month = today.toLocaleString("en-us", { month: "long" });
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    console.log("this.state.cityData");
+    console.log("this.state.weatherData: ", this.state.weatherData);
     return (
       <div className="App">
         <button onClick={this.locateMe}>Locate Me</button>
@@ -61,17 +69,29 @@ class App extends Component {
         <div>
           {days[today.getDay()]} {month} {today.getDate()}, {today.getFullYear()}
         </div>
+
         {this.state.weatherData ? (
-          <div>{this.state.weatherData.currently.temperature.toFixed(0)}</div>
+          <div>
+            <ReactAnimatedWeather
+              icon={this.getIcon(this.state.weatherData.currently.icon)}
+              color={defaults.color}
+              size={200}
+              animate={defaults.animate}
+            />
+            <div>{this.state.weatherData.currently.temperature.toFixed(0)}</div>
+            <div>{this.state.weatherData.currently.summary}</div>
+          </div>
         ) : (
-          <div>59</div>
+          <div>
+            <ReactAnimatedWeather
+              icon="RAIN"
+              color={defaults.color}
+              size={200}
+              animate={defaults.animate}
+            />
+            <div>59</div>
+          </div>
         )}
-        <ReactAnimatedWeather
-          icon={defaults.icon}
-          color={defaults.color}
-          size={100}
-          animate={defaults.animate}
-        />
       </div>
     );
   }
